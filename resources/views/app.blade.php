7c075da3575d79e307fe1aae774c2832689f77c4
Teodora Odanic <t.odanic@emakina.com>
2022-01-09 15:21:12 +0100

    git test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="keywords" content="ecomponents, ecomponents doo, helukabel, cables, wire harnessing, jst assemblies, pcbs, hardware, led, wires and cables, connectors, cable accessories, components">
-        <meta name="description" content="Tailor-made solutions in the field of wire harnesses, home appliance industry, gaming and vending machines industry" />
-        <meta name="googlebot" content="ecomponents doo">
-        <meta name=”robots” content="index, follow">
-        <meta name="google-site-verification" content="google-site-verification=A6CBjKijAyaMH5AjG4G2hYyv0JArWO-8BiyzoCCY1uc">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
-        <!-- <link rel="preload" crossorigin="anonymous" as="font" href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"> -->
-        <link rel="canonical" href="https://ecomponentsdoo.com/">
 
-        <title>Ecomponents doo</title>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords"
+        content="Ecomponents, ecomponents doo, helukabel, cables, wire harnessing, jst assemblies, pcbs, hardware, led, wires and cables, connectors, cable accessories, components">
+    <meta name="description"
+        content="Tailor-made solutions in the field of wire harnesses, home appliance industry, gaming and vending machines industry" />
+    <meta name="googlebot" content="ecomponents doo">
+    <meta name=”robots” content="index, follow">
+    <meta name="google-site-verification"
+        content="google-site-verification=A6CBjKijAyaMH5AjG4G2hYyv0JArWO-8BiyzoCCY1uc">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <!-- <link rel="preload" crossorigin="anonymous" as="font" href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"> -->
+    <link rel="canonical" href="https://ecomponentsdoo.com/">
+
+    <title>Ecomponents doo</title>
+</head>
+
+<body>
 
     <div id="app"></div>
 
-        <script rel="preload" src="/js/app.js"></script>
+    <script rel="preload" src="/js/app.js"></script>
 
-    </body>
+</body>
+
 </html>
