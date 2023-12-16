bc468c1cac5886addfc7b28c03e6c703e7bf58cd
Teodora Odanic <t.odanic@emakina.com>
2023-12-16 18:27:42 +0100

    contact form bug fix

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -8,20 +8,40 @@ use Illuminate\Support\Facades\Storage;
 
 class AppController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-	public function index(Request $request) {
+  /**
+   * @param Request $request
+   * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+   */
+  public function index(Request $request)
+  {
 
-	    return view('app');
-	}
+    return view('app');
+  }
 
-    /**
-     * @param Request $request
-     */
-	public function email(Request $request) {
-	    
-        event(new EmailEvent($request->email, $request->name, $request->phone, $request->country, $request->message, $imgPath));
+  /**
+   * @param Request $request
+   */
+  public function email(Request $request)
+  {
+    $imgPath = '';
+
+    if ($request->has('images')) {
+      $images = $request->images;
+
+      if (!is_string($images)) {
+        $imgPath = [];
+
+        foreach ($images as $image) {
+          $imgName = 'attachment-' . time() . rand(1, 10000) . '.' . $image->getClientOriginalExtension();
+          $path = $image->storeAs('uploads', $imgName);
+          $imgPath[] = $path;
+        }
+      } else {
+        $imgName = 'attachment-' . time() . '.' . $images->getClientOriginalExtension();
+        $imgPath = $images->storeAs('uploads', $imgName);
+      }
     }
+
+    event(new EmailEvent($request->email, $request->name, $request->phone, $request->country, $request->message, $imgPath));
+  }
 }
